e8c13172b6bce65e2000e48aca56136ee8eb6bf2
Trevor Le <tle@zephyrsleep.com>
2020-10-15 18:27:32 -0600

    Added medium level difficulty

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,42 +3,51 @@ window.addEventListener('load', main);
 let game = new MSGame()
 
 function main() {
-    game.init(8, 10, 10);
+    document.querySelectorAll(".menuButton").forEach( (button) => {
+        const rows = button.getAttribute("rows");
+        const cols = button.getAttribute("cols");
+        const mines = button.getAttribute("mines");
+        button.addEventListener("click", menu_button_click.bind(null, rows, cols, mines));
+    });
+    menu_button_click(8, 10, 10); // default 8x10, 10 mines
+}
+
+function menu_button_click(rows, cols, mines) {
+    game.init(rows, cols, mines);
     prepare_dom();
     render();
 }
 
 function render() {
     const grid = document.querySelector(".grid");
+    const gameRendering = game.getRendering();
     grid.style.gridTemplateColumns = `repeat(${game.ncols}, 1fr)`;
-    for (let i = 0; i < grid.children.length; i++) {
+    for (let i = 0; i < game.nrows * game.ncols; i++) {
         const cellRow = get_cell_row(i);
         const cellCol = get_cell_col(i);
-        render_cell(cellRow, cellCol, i);
+        const cellValue = gameRendering[cellRow][cellCol];
+        render_cell(i, cellValue);
     }
     update_flag_count();
 }
 
-function render_cell(row, col, i) {
-    const rendering = game.getRendering();
-    const grid = document.querySelector(".grid");
-    const cellRendering = rendering[row][col];
+function render_cell(i, cellValue) {
+    const cell = document.querySelector(".grid").childNodes[i];
 
-    if (game.exploded && cellRendering === "M") // game has exploded and cell is a mine
-        grid.childNodes[i].style.backgroundColor = "red"
+    if (game.exploded && cellValue === "M") // game has exploded and cell is a mine
+        cell.style.backgroundColor = "red"
 
-    if(cellRendering === "H") // cell is hidden
-        grid.childNodes[i].style.backgroundImage = "none";
+    if(cellValue === "H") // cell is hidden
+        cell.style.backgroundImage = "none";
 
-    if(cellRendering === "F") // cell is marked
-        grid.childNodes[i].style.backgroundImage = "url(img/flag.svg)";
+    if(cellValue === "F") // cell is marked
+        cell.style.backgroundImage = "url(img/flag.svg)";
 
-    if(cellRendering != "H" && cellRendering != "F" && cellRendering != "M") // cell is a number 0-9
-    {
-        if (cellRendering != 0)
-            grid.childNodes[i].innerHTML = cellRendering
-        grid.childNodes[i].style.backgroundImage = "none";
-        grid.childNodes[i].style.backgroundColor = "blue";
+    if(cellValue != "H" && cellValue != "F" && cellValue != "M") { // cell is a number 0-9 
+        if (cellValue != 0)
+            cell.innerHTML = cellValue
+        cell.style.backgroundImage = "none";
+        cell.style.backgroundColor = "blue";
     }
 }
 
@@ -53,8 +62,9 @@ function update_flag_count() {
 
 function prepare_dom() {
     const grid = document.querySelector(".grid");
-    const nCells = game.nrows * game.ncols; 
-    for (let i = 0; i < nCells ; i++) {
+    grid.innerHTML = ""; // remove all cells in grid
+    const nCells = game.nrows * game.ncols;
+    for (let i = 0; i < nCells ; i++) { // create cells
         const cell = document.createElement("div");
         cell.className = "cell";
         cell.setAttribute("cellIndex", i);
